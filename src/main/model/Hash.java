2af7e64d305943a2c369a07a4f478d3f510f5284
99644577 <betame@students.cs.ubc.ca>
2024-10-10 19:09:22 -0700

    Implemented Hash Class and Updated Hashdex Class (fixed some mistakes)

--- src/main/model/Hash.java
+++ src/main/model/Hash.java
@@ -1,6 +1,154 @@
 package main.model;
 
+// imports
+import java.util.*; //imports whole java util library
 //the objects that will be added to closet
+
 public class Hash {
+
+    // Attributes
+    private String material; // Material of the item (wool, cotton, etc)
+    private String colour; // Name of the item
+    private String name; // Name of the item
+    private String type; // Type of the item (shirt, pants, shoes, etc)
+    private List<String> tags; // Tags to describe the style (casual, formal, etc)
+    private boolean liked; // Whether the item was liked (saved) by the user
+
+    // Constructors
+    /*
+     * EFFECTS: creates a hash (clothing item) with given name, colour, material,
+     * types all
+     * set as empty strings
+     * initaliizes tags as an empty arrayList and sets liked to false
+     */
+    public Hash() {
+        this.name = "";
+        this.type = "";
+        this.colour = "";
+        this.material = "";
+        this.liked = false; // item is not saved in
+        this.tags = new ArrayList<>();
+    }
+
+    // Constructors
+    /*
+     * EFFECTS: creates a hash (clothing item) with given name, colour, material,
+     * types.
+     * initaliizes tags as an empty arrayList and sets liked to false
+     */
+    public Hash(String name, String type, String colour, String material) {
+        this.name = name;
+        this.type = type;
+        this.colour = colour;
+        this.material = material;
+        this.liked = false;
+        this.tags = new ArrayList<>();
+
+    }
+
+    // ---------SETTERS---------
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setLiked(Boolean liked) {
+        this.liked = liked;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    // ---------GETTERS---------
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public boolean getLiked() {
+        return liked;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    // *---------METHODS---------*
+    /*
+     * MODIFIES: this
+     * EFFECTS: likes hash by changing it's state
+     */
+    public boolean likedHash() {
+        setLiked(true);  //sets liked to true
+        System.out.println(getName() + " hash has been liked!");
+        return this.liked;
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: unlikes hash by changing it's state
+     */
+    public boolean unLikedHash(){
+        setLiked(false);  //set liked to false
+        System.out.println(getName() + " hash has be UNliked!");
+        return this.liked;
+    }
+
+    /*
+     * EFFECTS: displays the tags saved in the arrayList
+     */
+    public void displayTags(){
+        for (String e: tags){
+            System.out.println(e);
+        }
+    }
+
+    public void addTag(String tags){
+        this.tags.add(tags);  //adds a inputed tag to arraylist
+        System.out.println("Tag has been added SUCCESSFULLY...");
+    }
+
+    /*
+     * REQUIRES: a non-empty list
+     * MODIFIES: this, tags
+     * EFFECTS: removes most recently added tag
+     */
+    public void removeTag(){
+        this.tags.remove(tags.size()-1);  // removes the most recently added element
+        System.out.println("Tag has been removed");
+    }
+
+    /*
+     * REQUIRES: a non-empty list
+     * MODIFIES: this, tags
+     * EFFECTS: removes tag at specified index
+     */
+    public void removeTag(int index){
+        this.tags.remove(index);   //removed ibject at specified index
+        System.out.println("Tag  at index:"+ index + " has been removed");
+    }
 
 }
--- src/main/model/Hashdex.java
+++ src/main/model/Hashdex.java
@@ -1,121 +1,137 @@
 package main.model;
-import java.util.*;
 
-//a veritual closet that can be customized to make a uniquely themed closet
+import java.util.*;  //imports whole java util library
+
+//a virtual closet that can be customized to make a uniquely themed closet
 public class Hashdex {
+
     // Attributes
-    private String name; // name of hashdex
-    private String colour; // colour (hashdex image) of hashdex
-    private LinkedList <Hash> hashList; // LinkedList for the hashdex (allows hashes to be add, stored, etc)
+    private String name;               // name of hashdex
+    private String colour;             // colour (hashdex image) of hashdex
+    private LinkedList<Hash> hashList; // LinkedList for the hashdex (allows hashes to be add, stored, etc)
 
     // Constructors
     /*
-     * 
-     * EFFECTS: creates a blank (default) Hashdex with attributes set to blank (relative to the type)
+     * EFFECTS: creates a blank (default) Hashdex with attributes set to blank
+     * (relative to the type)
      * name is set to "Untitled"; colour is set to "white"
      * empty linkedlist is implemented
      */
-    public Hashdex(){
-        this.name = "Untitled";
-        this.colour = "white";
+    public Hashdex() {
+        this.name = "Untitled";  // sets name to a default string to signify basic constructor imple
+        this.colour = "white";   // se
     }
 
     /*
      * REQUIRES: name and colour to have a non-zero length
-     * 
-     * EFFECTS: creates a closet (hashdex) with the desired features which are set as it's attributes
+     * EFFECTS: creates a closet (hashdex) with the desired features which are set
+     * as it's attributes
      * parameter are set to their correlating attribute
-     * empty linkedlist is implemented
+     * initializes hashList as an empty linkedlist
      */
-    public Hashdex(String name, String colour){
-        this.hashList = new LinkedList <>();
+    public Hashdex(String name, String colour) {
+        this.hashList = new LinkedList<>();
         this.name = name;
         this.colour = colour;
     }
 
-    //---------SETTERS---------
-    public void setName(String name){
+    // ---------SETTERS---------
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setColour(String colour){
+    public void setColour(String colour) {
         this.colour = colour;
     }
 
-    public void setLinkedList(LinkedList hashList){
+    public void setLinkedList(LinkedList<Hash> hashList) {
         this.hashList = hashList;
     }
 
-    //---------GETTERS---------
-    public String getName(){
+    // ---------GETTERS---------
+    public String getName() {
         return name;
     }
 
-    public String getColour(){
+    public String getColour() {
         return colour;
     }
 
-    public LinkedList getHashList(){
+    public LinkedList<Hash> getHashList() {
         return hashList;
     }
 
-    //---------METHODS---------
+    // *---------METHODS---------*
     // Organize list by name
     /*
      * REQUIRES: a non-empty list
-     * MODIFIES: this, hashList
+     * MODIFIES: this
      * EFFECTS: sorts LinkedList in a alphabetical order
      */
-    public void sortByName(){
-        hashList.sort(null);//sorts the list alphabetically
-        //Collections.sort(hashList, Comparator.comparing(Hashdex::getName));
+    public void sortByName() {
+        hashList.sort(null);// sorts the list alphabetically
+        // Collections.sort(hashList, Comparator.comparing(Hashdex::getName));
     }
-
-    // public void sortBy(){
-    //     Collections.sort(hashList, Comparator.comparing(Hashdex::getName));
-    // }
 
     /*
      * REQUIRES: a instance of the hash class
      * MODIFIES: this, hashList
-     * EFFECTS: adds a give hash to the hashList (closet)
+     * EFFECTS: add hash to hashdex
      */
-    public void addToList(Hash e){// e stands for
+    public void addHash(Hash e) {// e stands for
         hashList.add(e);
     }
 
     /*
      * REQUIRES: a non-empty list
      * MODIFIES: this, hashList
-     * EFFECTS: removes the last element in the from the LinkedList
+     * EFFECTS: removes the last hash from the hashdex
      */
-    public void remove(){
+    public void removeHash() {
         hashList.removeLast();
     }
 
     /*
-     * REQUIRES: a non-empty list
+     * REQUIRES: a index greater than or equal 0 and less than hashList.size-1
      * MODIFIES: this, hashList
      * EFFECTS: removes a hash at a specific index from the List
      */
-    public void remove(Index i){
-        hashList.remove(i);
+    public void remove(int index) {
+        hashList.remove(index);
     }
 
     /*
      * REQUIRES: a non-empty list
-     * 
-     * EFFECTS: displays the elements of the linkedlist 
+     * EFFECTS: displays the elements of the linkedlist
      */
-    public void displayList(){
-        for (Hash h: hashList){//cycles through every element in the list
+    public void displayList() {
+        for (Hash h : hashList) {// cycles through every element in the list
             System.out.println(h);// prints the elements in the lsit
         }
     }
 
-    
-    
+    /*
+     * MODIFIES: this, hashList
+     * EFFECTS: adds hash to hashList if hash is liked
+     */
+    public void saveHash(Hash hash) {
+        if (hash.getLiked()) { // Only add if liked
+            hashList.add(hash);  // gets hashlist and adds 
+            System.out.println(hash.getName() + " has been added to " + this.name);
+        } else {
+            System.out.println(hash.getName() + " has not been liked yet.");
+        }
+    }
 
+
+
+    /*
+     * EFFECTS: displays hashdex name and it's hashes
+     */
+    @Override
+    public String toString() {
+        return "Hashdex: " + name + " - Items: " + hashList.toString();
+    }
 
 
 }
